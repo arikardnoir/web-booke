b9fc394677c3c9494f2f622941f20221b7d7e84a
arikardnoir <aristo.lopes@gmail.com>
2018-08-08 18:52:55 -0300

    Deploy Angular

---
+++ server.js
@@ -0,0 +1,18 @@
+const express= require('express'),
+      path = require('path');
+
+
+const app = express();
+
+app.use(express.static('./dist/booke'));
+
+app.get('/*', (req,res)=>{
+
+    res.sendFile(path.join(__dirname,'/dist/booke/index.html'));
+
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+
+    console.log('Server Started');
+})
