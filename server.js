1894092f55e8706036e7acd3a5c0799c7ec3d6f1
arikardnoir <aristo.lopes@gmail.com>
2018-08-08 22:44:58 -0300

    Deploy Git Heroku

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/app-booke'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + './src/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/app-booke/index.html'));
+    //res.sendFile(path.join(__dirname + './src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
